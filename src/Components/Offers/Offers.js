b2f6834d91176b349c8d35b54a3177df43e9b2bc
Katarzyna Szewczyk-Skrzynecka <k.szewczyk.skrzynecka@gmail.com>
2023-10-13 11:11:32 +0200

    add class name to paragraphs

--- src/Components/Offers/Offers.js
+++ src/Components/Offers/Offers.js
@@ -13,24 +13,16 @@ const Offers = () => {
             {offers.map((item) => {
               return (
                 <div className={styles.offerItem}>
-                  <p>{item.name}</p>
-                  {item.isNew === true ? <p>(nowość)</p> : null}
+                  <p className={styles.paragraphs}>{item.name}</p>
+                  {item.isNew === true ? (
+                    <p className={styles.paragraphs}>(nowość)</p>
+                  ) : null}
                   {item.isNew === true ? (
                     <div className={styles.redDot}></div>
                   ) : null}
                 </div>
               );
             })}
-            {/* <div className="offer-item">
-              <p>Usługa 1</p>
-              <p>(nowość)</p>
-              <div className="red-dot"></div>
-            </div>
-            <div className="offer-item">Usługa 2</div>
-            <div className="offer-item">Usługa 3</div>
-            <div className="offer-item">Usługa 4</div>
-            <div className="offer-item">Usługa 5</div>
-            <div className="offer-item">Usługa 6</div> */}
           </div>
         </section>
       </div>
